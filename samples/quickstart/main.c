79e5d576b9f492ecbc9673d60d457cf6145c76a1
Chen Xiangyu <xiangyu.chen@aol.com>
2017-02-21 16:40:46 +0800

    [MQUE0004][REBYPASS] update comments

---
+++ samples/quickstart/main.c
@@ -0,0 +1,18 @@
+/***********************************************************************
+*
+*	MSGQUE Sample  main.c
+*
+*
+************************************************************************/
+#include "msgque.h"
+#include "public.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc,char **argv)
+{
+
+	return 0;
+}
